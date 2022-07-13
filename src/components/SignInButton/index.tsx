bdbe14deb6eda55238916726915099c8445c93e3
Caslu-Faria <lucasglf.contato@gmail.com>
2022-07-13 10:52:33 -0300

    projeto next v2 - sign in button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,10 +1,22 @@
-import {FaGithub} from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
 import styles from './styles.module.scss'
 
 function SignInbutton() {
-    return (
-        <button className={styles.signInButton} type="button">
-            <FaGithub />
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn ? (
+
+        <button type="button" className={styles.signInButton}>
+            <FaGithub color="#84d361"/>
+            Lucas G Faria
+            <FiX className={styles.closeIcon} color="#737380"/>
+        </button>
+
+    ) : (
+        
+        <button type="button" className={styles.signInButton} >
+            <FaGithub color="#eba417"/>
             Sign in with Github
         </button>
     );
